9ee9258db87a88c3793a00273aceec40398f5661
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-15 18:27:20 +0800

    Create typedef.dart

---
+++ typedef.dart
@@ -0,0 +1,22 @@
+class Sum{
+  int first;
+  int second;
+  
+  Sum(this.first, this.second);
+  
+  int call() => first + second;
+}   
+
+typedef Total = Sum;
+typedef Jumlah = Sum;
+
+//import 'Sum.dart';
+
+void main(){
+  var jumlah = Jumlah(10, 10);
+  print(jumlah());
+  
+  var total = Total(10, 10);
+  print(total());
+  
+}
