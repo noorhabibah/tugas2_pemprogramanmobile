7bd3c17725a122bc3dc1d38f70be5388d7c61a5b
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-15 17:07:26 +0800

    Create mixin.dart

---
+++ mixin.dart
@@ -0,0 +1,33 @@
+abstract class Multimedia{}
+
+mixin Playable on Multimedia{
+  String? name;
+  
+  void play(){
+    print('Play $name');
+  }
+}
+
+mixin Stoppable on Multimedia{
+  String? name;
+  
+  void stop(){
+    print('Stop $name');
+  }
+}
+
+class Video extends Multimedia with Playable, Stoppable{}
+
+class Audio extends Multimedia with Playable, Stoppable{}
+
+void main(){
+  var video = Video();
+  video.name = "Tutor OOP";
+  video.play();
+  video.stop();
+  
+  var audio = Audio();
+  audio.name = "Tutorial OOP";
+  audio.play();
+  audio.stop();
+}
