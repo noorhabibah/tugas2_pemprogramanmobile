5a18ef71329b518c9043586b84fd9354c33b3fee
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-15 15:58:35 +0800

    Create getter_setter_expression_body.dart

---
+++ getter_setter_expression_body.dart
@@ -0,0 +1,22 @@
+class Rectangle{
+  int _width = 0;
+  int _length = 0;
+  
+  int get width => _width;
+  
+  set width(int value) => _width = value;
+  
+  int get length => _length;
+  
+  set length(int value) => _length = value;
+}
+//import 'Rectangle.dart';
+
+void main(){
+  var rectangle = Rectangle();
+  rectangle.width = 100;
+  print(rectangle.width);
+  
+  rectangle.length = 200;
+  print(rectangle.length);
+}
