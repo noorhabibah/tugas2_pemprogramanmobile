30bceceba24af3cce155855929347fb03d12904e
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-15 18:39:37 +0800

    Create static_field.dart

---
+++ static_field.dart
@@ -0,0 +1,10 @@
+class Application{
+  static final String author = "Noor habibah";
+  static final String name = "Tutor Pemprograman Dart";
+  
+}
+
+void main(){
+  print(Application.name);
+  print(Application.author);
+}
