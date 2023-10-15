e63ef3fac9e952e993b939a4c01a2173356c9d9f
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-15 21:05:01 +0800

    Update exception.dart

--- exception.dart
+++ exception.dart
@@ -1,19 +1,23 @@
-class ValidationException implements Exception{
+class ValidationException implements Exception {
   String message;
-  
-  ValidationException((this.message));
+
+  ValidationException(this.message);
 }
 
-class Validation{
-  static void validate(String username, String password){
-    if(username == ""){
-      throw ValidationException(("Username is blank"));
-    } else if(password == "");
+class Validation {
+  static void validate(String username, String password) {
+    if (username == "") {
+      throw ValidationException("Username is blank");
+    } else if (password == "") {
       throw ValidationException("Password is blank");
+    } else if (username != 'eko' || password != 'eko') {
+      throw Exception('Login failed');
+    }
+    // valid
   }
 }
 
-void main(){
+void main() {
   try{
     Validation.validate("Bibah", "");
   } on ValidationException{
