8789400db9024eb0a5fa7c780a22ab601c816986
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-15 18:19:56 +0800

    Create callable_class.dart

---
+++ callable_class.dart
@@ -0,0 +1,20 @@
+class Sum{
+  int first;
+  int second;
+  
+  Sum(this.first, this.second);
+  
+  int call(){
+    return first + second;
+}
+}
+
+//import 'Sum.dart';
+
+void main(){
+  var sum = Sum(10, 10);
+  
+  var total = sum();
+  print(total);
+  
+}
