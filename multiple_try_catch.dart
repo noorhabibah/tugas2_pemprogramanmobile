d4289160fd263d2fd24ac1765ffaef78839ee7ca
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-15 21:10:54 +0800

    Create multiple_try_catch.dart

---
+++ multiple_try_catch.dart
@@ -0,0 +1,28 @@
+class ValidationException implements Exception {
+  String message;
+
+  ValidationException(this.message);
+}
+
+class Validation {
+  static void validate(String username, String password) {
+    if (username == "") {
+      throw ValidationException("Username is blank");
+    } else if (password == "") {
+      throw ValidationException("Password is blank");
+    } else if (username != 'eko' || password != 'eko') {
+      throw Exception('Login failed');
+    }
+    // valid
+  }
+}
+
+void main() {
+  try{
+    Validation.validate("Bibah", "");
+  } on ValidationException catch (exception){
+    print("Error : ${exception.message}");
+  } on Exception catch (exception){
+    print("Error : ${exception.toString()}");
+  }
+}
